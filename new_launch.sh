f87867e58f0388abae552e872bd766df2e8a9d16
Sean Lynch <seanlynch@gmail.com>
2016-06-01 17:11:43 -0700

    switch to using python -m flask run

--- new_launch.sh
+++ new_launch.sh
@@ -1,4 +1,6 @@
-FLASK_APP=server/__init__.py FLASK_DEBUG=1 flask run
+FLASK_APP=server FLASK_DEBUG=1 python -m flask run # <-- Works!
+
+# export PYTHONPATH=pwd # <-- doesn't work, see: https://github.com/pallets/flask/issues/1847#issuecomment-223157715
 
 # FLASK_APP=server FLASK_DEBUG=1 flask run # <-- ImportError - No module named server
 # FLASK_APP=server/__init__.py FLASK_DEBUG=1 flask run # <-- won't actually load the views
